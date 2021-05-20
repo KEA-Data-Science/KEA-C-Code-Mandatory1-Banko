05607e63f63956c6e18e03086e00aa7182791abb
kiancn <kian.tema@gmail.com>
2021-05-20 11:50:38 +0200

    Full project upload

---
+++ bankoboard.h
@@ -0,0 +1,13 @@
+//
+// Created by Buddha on 19-05-2021.
+//
+
+#ifndef BANKO1_BANKOBOARD_H
+#define BANKO1_BANKOBOARD_H
+
+// bankoboard.h
+
+
+
+
+#endif //BANKO1_BANKOBOARD_H
---
+++ miscmath.h
@@ -0,0 +1,8 @@
+//
+// Created by Buddha on 19-05-2021.
+//
+
+#ifndef BANKO1_MISCMATH_H
+#define BANKO1_MISCMATH_H
+
+#endif //BANKO1_MISCMATH_H
